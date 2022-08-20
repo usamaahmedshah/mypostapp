f6876b339a491f4be1c1cf302c58d8c7fdbae1cf
USAMA <usamaahmadshah06@gmail.com>
2022-08-20 15:36:57 +0500

    incomplete

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import NavBar from './components/NavBar';
+import About from './screens/About';
+import Contact from './screens/Contact';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './screens/Home';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div>
+        <NavBar></NavBar>
+        <Routes>
+          <Route path='/' element={<Home />}></Route>
+          <Route path='/about' element={<About />}></Route>
+          <Route path='/contact' element={<Contact />}></Route>
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
